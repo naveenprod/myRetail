492445cb155caaf04f2d339477966a7f0ee4043d
adwitheasenaveen <adwitheasenaveen@gmail.com>
2017-06-30 09:57:32 +0530

    changes for put verb

--- src/main/java/module/product/services/ProductService.java
+++ src/main/java/module/product/services/ProductService.java
@@ -1,7 +1,10 @@
 package module.product.services;
 
+import module.product.domain.Product;
+
 public interface ProductService {
 	
 	public String getProduct(String id); 
+	public String updateProduct(Product toBeUpdate);
 	
 }
